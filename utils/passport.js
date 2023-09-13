5a7e29d70678e55aa154b153bec3b5035a18e7d9
Rohit Tiwari <137717184+rohittiwari-dev@users.noreply.github.com>
2023-09-13 23:38:18 +0530

    Update passport.js

--- utils/passport.js
+++ utils/passport.js
@@ -7,7 +7,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/api/v1/auth/google/callback",
+            callbackURL: "https://schedular-backend.onrender.com/api/v1/auth/google/callback",
             scope: [
                 "openid",
                 "profile",
